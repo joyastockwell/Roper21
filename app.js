90b66dda5e02b670547c311e7627e0a9848620bf
joyastockwell <stockwja@rose-hulman.edu>
2021-07-06 09:17:23 -0700

    Got cow retrieved

--- app.js
+++ app.js
@@ -4,45 +4,35 @@ var storage = firebase.storage();
 
 var storageRef = storage.ref();
 
-async function getCowData(doc)  {
+async function sortCowUpdates()  {
     const lastSortedData = await db.collection('Last DataUpdate Sorted').doc('0').get();
     if (!lastSortedData.exists) {
         console.log('Last DataUpdate sorted unknown');
     }
     const lastSortedID = lastSortedData.data()["last number"];
+    console.log(lastSortedID);
 
-    const UnsortedDataUpdates = await db.collection('DataUpdates').where(firebase.firestore.FieldPath.documentId(), ">", lastSortedID).get();
-    console.log(UnsortedDataUpdates);
+    //const UnsortedDataUpdates = await db.collection('DataUpdates').where(firebase.firestore.FieldPath.documentId(), ">", lastSortedID).get();
+    const UnsortedDataUpdates = await db.collection('DataUpdates').where(firebase.firestore.FieldPath.documentId(), ">", "202106231350475844").get();
     UnsortedDataUpdates.docs.forEach((doc) => {
-        console.log("dream o' me")
-        console.log(doc.data());
+        // get cow of same serial number
+        var currSerialNumber = doc.data()["Serial Number"];
+        console.log(currSerialNumber);
+        db.collection("Serial Number Cows").where('currSerialNumber', '==', currSerialNumber).get().then((data) => {
+            if(data.size < 1) {
+                console.log("Serial number not found");
+                return;
+            }
+            data.forEach(cow => {
+                console.log(cow.data().name);
+            });
+            if(data.size > 1) {
+                console.log("Warning. Multiple cows with " + currSerialNumber + " as current serial number. Using data for only " + cow.name);
+            }
+
+        });
+        // store serial number in serial numbers collection
     });
-
-
-    // Not very elegant, but I searched for an hour for an alternative and couldn't find one!
-    await db.collection('prototype cows').doc(doc.id).get().then(snapshot => {
-     });
 }
 
-function renderCow(doc) {
-    let li = document.createElement('li');
-    let cowData = document.createElement('span');
-
-    
-    // retrieve tags
-    getCowData(doc).then(str => {
-        cowData.textContent = str;
-    });
-
-    li.appendChild(cowData);
-    itemList.appendChild(li);
-}
-
-
-// renders each item in the Items collection in Firebase
-db.collection('Serial Number Cows').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        console.log("name: " + doc.data().name);
-        renderCow(doc);
-    })
-})
+sortCowUpdates();
